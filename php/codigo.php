1c3dcea39cf3013b0d2ac605d68ca57c340651c7
Florencia <florenciamorelliIT@gmail.com>
2024-07-04 12:25:32 -0300

    php should be working but the domain on InfinityFree is still pending

--- php/codigo.php
+++ php/codigo.php
@@ -1,23 +1,22 @@
 <?php
-$to = "tumail@educacionit.com";
-$subject = "Mail desde el formulario";
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 $nombre = $_POST['nombre'];
 $email = $_POST['email'];
 $comentarios = $_POST['comentarios'];
-$message = "
-<html>
-<head>
-<title>HTML</title>
-</head>
-<body>
-<h1>Información del formulario</h1>
-<p>Nombre del usuario: $nombre </p> 
-<p>Email: $email </p> 
-<p>Comentarios: $comentarios </p> 
-</body>
-</html>";
- echo 'Gracias por comunicarse con nosotros';
-mail($to, $subject, $message, $headers);
-?>
+
+$to = $_POST['email'];
+$subject = "WWWapas - Prueba de formulario";
+
+$message = "Gracias por probar el formulario de contacto de WWWapas. Los datos ingresados fueron los siguientes: \nNombre: " . $nombre . "\nEmail: ". $email . "\nComentarios: " . $comentarios;
+
+if($email != null) {
+    if(mail($to, $subject, $message)) {
+        echo 'Gracias por probar el formulario. Revise su casilla de correo.';
+        header('Location: index.html');
+    } else {
+        echo 'Error al enviar el correo.';
+    }
+} else {
+    echo 'El campo de email está vacío.';
+}
+
+exit();
